41768c3d8ba91efe54300ae787488f9b8cd38f95
Altiansyah Fanny <altiansyah.fanny@asiatechintiraya.com>
2022-12-26 14:55:32 +0700

    create form for handle input search in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Navbar from '../components/Navbar';
 import { selectRepositoryState } from '../store/repositorySlice';
 
 const Home = () => {
+	const dispatch = useDispatch();
+
+	const [username, setUsername] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+
 	const repositories = useSelector(selectRepositoryState);
 	console.log('repositories : ', repositories);
+
 	return (
 		<>
 			<Navbar />
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<main className="px-8 mt-4">
+				<div className="flex justify-center">
+					<form>
+						<div className="border rounded-lg overflow-hidden">
+							<input
+								type={'text'}
+								value={username}
+								onChange={(e) => setUsername(e.target.value)}
+								className="focus:outline-none px-3 text-gray-700"
+							/>
+							<button
+								type="submit"
+								className="bg-blue-500 text-white  px-5 py-1 transition hover:bg-blue-600"
+							>
+								Search
+							</button>
+						</div>
+					</form>
+				</div>
+				<hr className="my-4" />
+			</main>
 		</>
 	);
 };
