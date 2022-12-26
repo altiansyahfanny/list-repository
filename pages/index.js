d62d3a6f78304e29f43f1f86c62a44e8d76a0b43
Altiansyah Fanny <altiansyah.fanny@asiatechintiraya.com>
2022-12-26 14:58:30 +0700

    create card and implement serach repository by username github

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Card from '../components/Card';
 import Navbar from '../components/Navbar';
-import { selectRepositoryState } from '../store/repositorySlice';
+import { selectRepositoryState, setRepositoryState } from '../store/repositorySlice';
 
 const Home = () => {
 	const dispatch = useDispatch();
@@ -12,12 +13,57 @@ const Home = () => {
 	const repositories = useSelector(selectRepositoryState);
 	console.log('repositories : ', repositories);
 
+	const getListRepository = async (username) => {
+		setIsLoading(true);
+		try {
+			const response = await fetch(`https://api.github.com/users/${username}/repos`);
+			const responseJson = await response.json();
+
+			if (responseJson.message === 'Not Found') {
+				throw 'Username Not Found';
+			}
+
+			dispatch(setRepositoryState(responseJson));
+		} catch (error) {
+			alert(error ?? 'Something went wrong');
+			dispatch(setRepositoryState([]));
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	const handleSearch = (e) => {
+		e.preventDefault();
+		if (!username) {
+			return alert('Username is required!');
+		}
+		getListRepository(username);
+	};
+
+	let content;
+
+	if (isLoading) {
+		content = <p>Loading...</p>;
+	} else {
+		if ([...repositories].length) {
+			content = (
+				<div className="grid grid-cols-3 gap-4 mb-8">
+					{[...repositories].map((repository) => (
+						<Card key={repository.id} repository={repository} />
+					))}
+				</div>
+			);
+		} else {
+			content = <p>Data Not Found!</p>;
+		}
+	}
+
 	return (
 		<>
 			<Navbar />
 			<main className="px-8 mt-4">
 				<div className="flex justify-center">
-					<form>
+					<form onSubmit={handleSearch}>
 						<div className="border rounded-lg overflow-hidden">
 							<input
 								type={'text'}
@@ -35,6 +81,7 @@ const Home = () => {
 					</form>
 				</div>
 				<hr className="my-4" />
+				{content}
 			</main>
 		</>
 	);
